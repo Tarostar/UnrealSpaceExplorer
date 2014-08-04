07a675de3c1afa7d5845678faef9013a15c63df1
Claus Bornich <clausb@gmail.com>
2014-08-04 15:42:12 +0200

    removed intermiediate source files

--- Intermediate/Build/Win64/SpaceExplorerEditor/Development/SpaceExplorer/PCH.SpaceExplorer.SpaceExplorer.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "G:\Calus\Projects\Unreal\SpaceExplorer\Source\SpaceExplorer\Public\SpaceExplorer.h"
--- Intermediate/Build/Win64/SpaceExplorerEditor/Development/SpaceExplorer/SpaceExplorer.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_SPACEEXPLORER_H__
-#define __AUTO_SPACEEXPLORER_H__
-//Last Write: 04/08/2014 09:10:44 +00:00
-#include "G:\Calus\Projects\Unreal\SpaceExplorer\Source\SpaceExplorer\Public\SpaceExplorer.h"
-#endif//__AUTO_SPACEEXPLORER_H__
